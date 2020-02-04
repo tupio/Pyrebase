316031e86a9151a19716c8fecae5968d1f2b2d01
Magic Man <34391915+tupio@users.noreply.github.com>
2020-02-04 13:00:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Pyrebase',
-    version='3.0.27',
-    url='https://github.com/thisbejim/Pyrebase',
-    description='A simple python wrapper for the Firebase API',
+    name='Pyrebase-t',
+    version='1.0.28',
+    url='https://github.com/tupio/Pyrebase/',
+    description='A simple python wrapper for the Firebase API forked from https://github.com/thisbejim/Pyrebase',
     author='James Childs-Maidment',
     license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 1 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.7',
     ],
-    keywords='Firebase',
+    keywords='Firebase Wrapper',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'requests==2.11.1',
-        'gcloud==0.17.0',
+        'gcloud==0.18.3',
         'oauth2client==3.0.0',
-        'requests_toolbelt==0.7.0',
+        'requests_toolbelt==0.9.1',
         'python_jwt==2.0.1',
-        'pycryptodome==3.4.3'
+        'pycryptodome==3.9.6'
     ]
 )
